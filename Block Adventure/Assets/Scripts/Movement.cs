ea9586902b189f31e881db017454db8ea02f7b74
Haseeb Akbar <haseeb2000@hotmail.com>
2018-04-09 11:54:57 +0100

    Able to move player and and have camera follow.

---
+++ Block Adventure/Assets/Scripts/Movement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+	public float speed;
+
+	private Rigidbody rb;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * speed);
+	}
+
+
+	void Jump (){
+		if (Input.GetKeyDown ("space")) {
+			Vector3 playerPos = transform.position;
+			playerPos += new Vector3 (0f, 100f, 0f);
+			rb.AddForce (playerPos);
+		}
+	}
+}
+
+
+
+
+//
+//float moveHorizontal = Input.GetAxis ("Horizontal");
+//float moveVertical = Input.GetAxis ("Vertical");
+//
+//Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+//
+//rb.AddForce (movement * speed);
