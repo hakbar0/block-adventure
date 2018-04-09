230b7a4a040ed29840064ab021f31450ed222e6f
Haseeb Akbar <haseeb2000@hotmail.com>
2018-04-09 16:18:33 +0100

    Able to pick up items and increase score.

---
+++ Block Adventure/Assets/Scripts/ScoreKeeper.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreKeeper : MonoBehaviour {
+
+	private int score;
+
+	// Use this for initialization
+	void Start () {
+		score = 0;
+	}
+
+	void OnTriggerEnter(Collider other){
+
+		if (other.gameObject.CompareTag ("pickup")) {
+			score++;
+			print (score);
+		}
+	}
+	
+
+}
